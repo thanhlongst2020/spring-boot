9ffc80bbe50cbf440dd752e6ff3e9fd3895059e0
thanhlongst2020 <68694926+thanhlongst2020@users.noreply.github.com>
2022-09-10 23:15:39 +0700

    Add deleting and paginating new API

--- src/main/java/com/laptrinhjavaweb/service/impl/NewService.java
+++ src/main/java/com/laptrinhjavaweb/service/impl/NewService.java
@@ -1,6 +1,10 @@
 package com.laptrinhjavaweb.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.laptrinhjavaweb.converter.NewConverter;
@@ -39,5 +43,40 @@ public class NewService implements INewService  {
 		
 		return newConverter.toDTO(newEntity);
 	}
+
+	@Override
+	public void delete(long[] ids) {
+		for(long item: ids) {
+			newRepository.delete(item);
+		}
+	}
+
+	@Override
+	public List<NewDTO> findAll(Pageable pageable) {
+		List<NewDTO> results = new ArrayList<>();
+		List<NewEntity> entities = newRepository.findAll(pageable).getContent();
+		for(NewEntity item: entities) {
+			NewDTO newDTO = newConverter.toDTO(item);
+			results.add(newDTO);
+		}
+		return results;
+	}
+
+	@Override
+	public List<NewDTO> findAll() {
+		List<NewDTO> results = new ArrayList<>();
+		List<NewEntity> entities = newRepository.findAll();
+		for(NewEntity item: entities) {
+			NewDTO newDTO = newConverter.toDTO(item);
+			results.add(newDTO);
+		}
+		return results;
+	}
+	
+	@Override
+	public int totalItem() {
+		return (int) newRepository.count();
+	}
+
 
 }
